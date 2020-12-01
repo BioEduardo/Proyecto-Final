2690d9282045c9c31001635b337c95e8627d2458
BioEduardo <67594938+BioEduardo@users.noreply.github.com>
2020-11-30 19:19:55 -0600

    A01746664 Terminado
    Eduardo Joel Cortez Valente. Terminado

--- Informe.h
+++ Informe.h
@@ -3,18 +3,59 @@
 
 #include <iostream>
 #include "Estado.h"
+#include "Fecha.h"
+
 using namespace std;
 #include <string>
 
 class Informe{
     private:
-        string fecha_de_consulta;
+        Fecha fecha_de_consulta;
         string tipo_de_producto;
+        Estado estado;
     public:
     Informe() = default;
-    Informe(string fecha_de_consulta, string tipo_de_producto){
+    Informe(Fecha fecha_de_consulta, string tipo_de_producto, Estado estado){
         this->fecha_de_consulta = fecha_de_consulta;
         this->tipo_de_producto = tipo_de_producto;
+        this->estado = estado;
+    }
+
+    //int getDia(){return dia;}
+    Fecha getFechaConsulta(){return fecha_de_consulta;}
+    string getTipoDeProducto(){return tipo_de_producto;}
+
+    void setTipoDeProducto(string tipo_de_producto){this->tipo_de_producto = tipo_de_producto;}
+
+    void ver_fecha_de_consulta(){
+        int d, m, a;
+        //cout << "Seleccione la opcion que desea realizar: " ; cin >> op;
+        cout << "La consulta del estado de su paquete se esta haciendo el dia: "; cin >> d;
+        cout << "En el mes numero: "; cin >> m;
+        cout << "En el anio: "; cin >> a;
+        fecha_de_consulta.setDia(d);
+        fecha_de_consulta.setMes(m);
+        fecha_de_consulta.setAnio(a);
+        cout << "Fecha en que se esta realizando la consulta: " << fecha_de_consulta.getDia() << "/" << fecha_de_consulta.getMes() << "/" << fecha_de_consulta.getAnio() << endl;
+    }
+    void ver_fecha_de_entrega(){
+        cout << "Su paquete llegara en esta fecha: " << estado.getFechaEntrega().getDia() << "/" << estado.getFechaEntrega().getMes() << "/" << estado.getFechaEntrega().getAnio() << endl;
+    }
+    void notificar_condiciones(){
+        cout << "Las condiciones de su paquete son las siguientes: " << estado.getCondiciones() << endl;
+    }
+    void notificar_paqueteria(){
+        cout << "La paqueteria que se encuentra a cargo de su paquete es: " << estado.getPaqueteria() << endl;
+    }
+    void notificar_lugar(){
+        if ((fecha_de_consulta.getDia() == estado.getFechaEntrega().getDia()) && (fecha_de_consulta.getMes() == estado.getFechaEntrega().getMes()) && (fecha_de_consulta.getAnio() == estado.getFechaEntrega().getAnio())){
+            cout << "Felicidades. Su paquete le sera entregado hoy. Mantengase pendiente del portico de su casa" << endl;
+        } 
+        else
+        {
+            cout << "El producto se encuentra en: " << estado.getUbicacionProducto() << endl;
+            cout << "Si tiene alguna pregunta, puede ponerse en contacto con la empresa" << endl;
+        }
     }
 };
 
--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -4,12 +4,97 @@
 using namespace std;
 #include <string>
 
+void pruebaFecha(){
+    cout << "\nPrueba Fecha" << endl;
+    cout << "Para comprobar e utilizar todos los metodos de esta clase, se definira y desplegara la fecha de la independencia de Mexico" << endl;
+    Fecha festivo;
+    festivo.setDia(10);
+    festivo.setMes(9);
+    festivo.setAnio(1810);
+    cout << "Fecha: El dia es " << festivo.getDia() << ", el numero de mes " << festivo.getMes() << ", el anio " << festivo.getAnio() << endl;
+}
+void pruebaEstado(){
+    string cambio;
+    cout << "\nPrueba Estado" << endl;
+    cout << "La prueba de esta clase funciona como si usted fuera un trabajador que debe realizar lo que cada funcion le indica. Se utilizaron todos los metodos y atributos de la clase Estado" << endl;
+    Estado est1;
+    cout << "\n****Generar una fecha de salida" << endl;
+    est1.generar_fecha_salida();
+    cout << "\n****Generar una fecha de llegada" << endl;
+    est1.generar_fecha_llegada();
+    cout << "\n****Actualizar las condiciones" << endl;
+    est1.actualizar_condiciones();
+    cout << "La condicion en que sen encuentra el paquete es: " << est1.getCondiciones() << endl;
+    cout << "\n****Actualizar las ubicaciones" << endl;
+    est1.acutalizar_ubicacion();
+    cout << "La ubicacion en la que se encuentra el paquete es: " << est1.getUbicacionProducto() << endl;
+    cout << "\n****Probar que los metodos set y get de paqueteria funcionen perfectamente" << endl;
+    cout << "Nueva paqueteria "; cin >> cambio;
+    est1.setPaqueteria(cambio);
+    cout << "La paqueteria actual es " << est1.getPaqueteria() << endl;
+}
+void pruebaInforme(){
+    string producto;
+    cout << "\nPrueba Informe" << endl;
+    cout << "La prueba de esta clase funciona como si usted fuera un usuario que desea ver como se esta desarrollando el envio." << endl;
+    cout << "Se desarrollo una clase de Estado previa, con la que Informe trabajara, para mayor comodidad. Se utilizaron todos los metodos y atributos de la clase Informe" << endl;
+    Estado estEjemplo{{30,11,2020},{1,1,2021},"FedEx","En la tienda","El paquete esta en perfectas condiciones"};
+    Informe compre{{2,11,2020},"Alimento",estEjemplo};
+    cout << "\n****Ver la fecha en que se hacer la consulta" << endl;
+    cout << "La fecha que se tiene contemplada es 30/11/2020. Ingrese la fecha en la que se encuentra actualmente" << endl;
+    compre.ver_fecha_de_consulta();
+    cout << "\n****Ver la fecha de entrega" << endl;
+    compre.ver_fecha_de_entrega();
+    cout << "\n****Notificar las condiciones del pedido" << endl;
+    compre.notificar_condiciones();
+    cout << "\n****Notificar la paqueteria por la que se esta transportando" << endl;
+    compre.notificar_paqueteria();
+    cout << "\n****Notificar el lugar en el que se encuentra el pedido" << endl;
+    compre.notificar_lugar();
+    cout << "\n****Probar que los get y set del tipo de producto funcionen con normalidad" << endl;
+    cout << "Nuevo tipo de producto: "; cin >> producto;
+    compre.setTipoDeProducto(producto);
+    cout << "El tipo de producto ahora es " << compre.getTipoDeProducto() << endl;
+
+}
 int main()
 {
-    cout << "\nPrueba numero 1" << endl;
-    Estado estado1;
-    estado1.generar_fecha_salida();
-    estado1.generar_fecha_llegada();
+    int op;
+    cout << "    Selecciona la Clase que desee probar\n";
+    cout << "<1> Clase Fecha (Parte individual de Eduardo Joel Cortez Valente - A01746664)\n";
+    cout << "<2> Clase Estado\n";
+    cout << "<3> Clase Informe\n";
+    cout << "<4> Clase Cliente\n";
+    cout << "<5> Clase Adquisicion\n";
+    cout << "<6> YARI AQUI PONES LA CLASE DE TU TRABAJO INDIVIDUAL :)\n";
+    cout << "<7> para salir del programa\n";
 
+    while (op != 7)
+    {   
+        cout << "\n ------ Opcion inicial: "; cin >> op;
+
+        switch (op) {
+        case 1:
+            pruebaFecha();
+            break;
+        case 2:
+            pruebaEstado();
+            break;
+        case 3:
+            pruebaInforme();
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        default:
+            cout << "\nSelecciona una opcion inicial valida de las 7 disponibles\n";
+            break;
+        }
+    }
     return 0;
 }
